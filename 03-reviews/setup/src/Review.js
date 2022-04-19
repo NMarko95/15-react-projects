177d8b1ee2e12459c2315a3a12ab50694e6776ee
NMarko <markonikolic735@yahoo.com>
2022-04-19 10:22:11 +0200

    Finished project 3

---
+++ 03-reviews/setup/src/Review.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  console.log(index);
+
+  const nextPerson = () => {
+    index === people.length - 1 ? setIndex(0) : setIndex(index + 1);
+  };
+
+  const prevPerson = () => {
+    index === 0 ? setIndex(people.length - 1) : setIndex(index - 1);
+  };
+
+  const randomPerson = () => {};
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button onClick={prevPerson} className="prev-btn">
+          <FaChevronLeft />
+        </button>
+        <button onClick={nextPerson} className="next-btn">
+          <FaChevronRight />
+        </button>
+      </div>
+      <button onClick={randomPerson} className="random-btn">
+        surprise me
+      </button>
+    </article>
+  );
+};
+
+export default Review;
