c39d99d345a1256d792bb6a585426f9080984fc6
NMarko <markonikolic735@yahoo.com>
2022-06-11 17:13:14 +0200

    Finished part of project 13

---
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -0,0 +1,29 @@
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
+
+const Hero = () => {
+  const { closeSubmenu } = useGlobalContext();
+
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            quasi adipisci minus eos? Quasi reiciendis aut quia mollitia sint
+            error maxime fugit natus, explicabo ducimus, perspiciatis
+            accusantium dignissimos nobis at!
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -0,0 +1,10 @@
+import React, { useState, useRef, useEffect } from "react";
+import { useGlobalContext } from "./context";
+
+const Submenu = () => {
+  const { isSubmenuOpen } = useGlobalContext();
+
+  return <aside className={`submenu ${isSubmenuOpen && "show"}`}></aside>;
+};
+
+export default Submenu;
